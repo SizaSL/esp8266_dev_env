28eac9ceaaa299a5f18c0e20193548dd52ec5e5d
Siza <sizasl@gmail.com>
2019-09-11 14:42:48 +1200

    Changes as follow: * Rename scripts * Add local setup for linux * Add sudo for esp8266 necessary tool installation * Setup synced folder in Vagrantfile * Make LF for line ending of .sh scripts

---
+++ dev_env/env_setup_linux_local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Setup env
+./provisioning/system_update.sh
+./provisioning/install_esp8266_tools.sh
+./provisioning/install_dev_tools.sh
--- dev_env/env_setup_win_vm.bat
+++ dev_env/env_setup_win_vm.bat
@@ -2,10 +2,10 @@ REM Install Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Install VirtualBox
-choco install virtualbox
+choco install -y virtualbox
 
 REM Install Vagrant
-choco install vagrant
+choco install -y vagrant
 
 REM Setup env
 vagrant up
